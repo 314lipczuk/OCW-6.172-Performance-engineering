719bc58c669a8d014e03678e2649cd89d3214a5a
314lipczuk <3.14lipczuk@gmail.com>
2024-03-17 17:04:08 +0100

    measuring things

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OCW-6.172-Performance-engineering
+
+My notes and implementations for an OCW course 6.172 
--- matrix_multiplication/main.zig
+++ matrix_multiplication/main.zig
@@ -5,23 +5,25 @@ const print = std.debug.print;
 const RandGen = std.rand.DefaultPrng;
 const Random = std.rand.Random;
 pub fn main() !void {
+    // just a simple matrix multiply program to look at
     const ally = std.heap.page_allocator;
     comptime var N = 1024;
-    var a = try Matrix(u32, N).init_random(ally);
-    var b = try Matrix(u32, N).init(ally, 0);
-    var c = try Matrix(u32, N).init(ally, 0);
+    var a = try Matrix(f32, N).init_random(ally);
+    var b = try Matrix(f32, N).init(ally, 0);
+    var c = try Matrix(f32, N).init(ally, 0);
     for (0..N) |i| {
         b.data[i * N + i] = 2;
     }
 
     const t1 = try std.time.Instant.now();
-    a.multiply_matrix(&b, &c);
-    //Matrix.multiply_matrix(&a, &b, &c);
+    a.multiply_matrix_with_ordering(&b, &c, 1);
     const t2 = try std.time.Instant.now();
     const time_ns = std.time.Instant.since(t2, t1);
     const time_ms: f32 = @as(f32, @floatFromInt(time_ns)) / @as(f32, 1_000_000);
     const time_s: f32 = @as(f32, @floatFromInt(time_ns)) / @as(f32, 1_000_000_000);
-    print("Timing for a {d}x{d} matrix multiply:\n{d:5.0001}ms\n{d:5.0001}s\n", .{ N, N, time_ms, time_s });
+    const operations_total = std.math.pow(f32, N, 3);
+    const flops = operations_total / time_s;
+    print("Timing for a {d}x{d} matrix multiply:\n{d:5.0001}ms\n{d:5.0001}s\nTotal operations: {e}\ntherefore FLOPS:{e}\n", .{ N, N, time_ms, time_s, @as(f32, operations_total), flops });
     a.deinit(ally);
     b.deinit(ally);
     c.deinit(ally);
@@ -31,9 +33,9 @@ test "Matrix row ordering " {
     // setup the matrices
     const ally = std.heap.page_allocator;
     comptime var N = 2048;
-    var a = try Matrix(u32, N).init_random(ally);
-    var b = try Matrix(u32, N).init(ally, 0);
-    var c = try Matrix(u32, N).init(ally, 0);
+    var a = try Matrix(f32, N).init_random(ally);
+    var b = try Matrix(f32, N).init(ally, 0);
+    var c = try Matrix(f32, N).init(ally, 0);
     for (0..N) |i| {
         b.data[i * N + i] = 2;
     }
@@ -49,6 +51,10 @@ test "Matrix row ordering " {
         print("\n--------------\nOrdering {d}:\n{d:5.0001}ms\n{d:5.0001}s\n--------------\n", .{ @as(u64, ordering), time_ms, time_s });
         c.multiply_scalar(0);
     }
+    // calc performance?
+    // flops - floating point operations per second
+    // how much operations are there in the matmul?
+    // O(N^3) in my current impl
     a.deinit(ally);
     b.deinit(ally);
     c.deinit(ally);
@@ -72,7 +78,7 @@ pub fn Matrix(comptime Contained: type, comptime N: comptime_int) type {
             var data: []Contained = try ally.alloc(Contained, N * N);
 
             for (0..data.len) |d| {
-                data[d] = Random.uintLessThan(rand.random(), Contained, 25);
+                data[d] = Random.float(rand.random(), Contained);
             }
             return Self{ .data = data };
         }
@@ -107,9 +113,9 @@ pub fn Matrix(comptime Contained: type, comptime N: comptime_int) type {
             assert(self.data.len == other.data.len);
             const a = self;
             const b = other;
-            for (0..N) |c_row| {
-                for (0..N) |c_col| {
-                    for (0..N) |i| {
+            for (0..N) |i| { //C
+                for (0..N) |c_row| { //A
+                    for (0..N) |c_col| { //B
                         result.data.ptr[c_row * N + c_col] += a.data[c_row * N + i] * b.data[c_col + N * i];
                     }
                 }
@@ -117,6 +123,7 @@ pub fn Matrix(comptime Contained: type, comptime N: comptime_int) type {
         }
 
         pub fn multiply_matrix_with_ordering(self: *Self, other: *Self, result: *Self, ordering: u64) void {
+            // this function can be executed with different loop orderings, to check their performance
             assert(self.data.len == other.data.len);
             const a = self;
             const b = other;
